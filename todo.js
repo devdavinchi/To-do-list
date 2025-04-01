c28c5c27bf435917f3726d1cdea7f33a3b69c8c3
dev <devdavinchi06@gmail.com>
2025-04-01 23:12:34 +0545

    version 1.0

---
+++ todo.js
@@ -0,0 +1,46 @@
+const inputField = document.getElementById("inputBox");
+const container = document.getElementById("listContainer");
+
+function addTask() {
+  if (inputField.value === "") {
+    alert("you must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputField.value;
+    container.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = '<img src="image/cross.png"/>';
+    li.appendChild(span);
+  }
+  inputField.value = "";
+  saveData();
+}
+
+inputField.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addTask();
+    saveData();
+  }
+});
+container.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "IMG") {
+      e.target.parentElement.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", container.innerHTML);
+}
+
+function getData() {
+  container.innerHTML = localStorage.getItem("data");
+}
+getData();
